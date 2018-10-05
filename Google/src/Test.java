e7948ff4485ac9cf32c28318e0e223ded5d1886f
Colin Barber <colinbmusic@gmail.com>
2018-10-05 12:48:03 -0400

    Refactored Directory Structure

--- Google/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-
-        int[][] maze = {{0,1,1,1,1,1}, {0,1,1,1,1,1}, {0,1,1,0,0,0}, {0,0,0,1,1,0}};
-
-        System.out.println(Answer4_3.answer(maze));
-    }
-}
--- Interview Questions/src/Test.java
+++
@@ -1,9 +0,0 @@
-/* Run test cases here
- */
-public class Test {
-    public static void main(String[] args) {
-        int[] arr = {1,1,1,5,0,5,3,2,2,5,7,8,8,8};
-
-        ArrayFindPairs.findPair(arr, 6);
-    }
-}
